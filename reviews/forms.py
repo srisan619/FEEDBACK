6d8dadae03f625917f5001460f75b19fa9e61c28
Srinivasan <srisan619@gmail.com>
2024-11-05 19:46:26 +0100

    store form data in databse

--- reviews/forms.py
+++ reviews/forms.py
@@ -5,4 +5,6 @@ class ReviewForm(forms.Form):
         "required": "Your name should not be empty",
         "max_length": "Please enter the shorter name"
     })
+    review_text = forms.CharField(label="Your feedback", max_length=200, widget=forms.Textarea)
+    rating = forms.IntegerField(label="Your rating", min_value=1, max_value=5)
     # captcha_answer = forms.IntegerField(label="2 + 2", label_suffix=" =")
--- reviews/views.py
+++ reviews/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render, HttpResponseRedirect
 from .forms import ReviewForm
+from .models import Review
 
 # Create your views here.
 def review(request):
     if request.method == "POST":
         form = ReviewForm(request.POST)
         if form.is_valid():
-            print(form.cleaned_data)
+            # print(form.cleaned_data)
+            review = Review(username=form.cleaned_data['username'], review_text=form.cleaned_data['review_text'], rating=form.cleaned_data['rating'])
+            review.save()
             return HttpResponseRedirect('/thank-you')
     else:
         form = ReviewForm()
