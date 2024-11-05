b0be455a0456bb20e6707ec4c85aac644062bc08
Srinivasan <srisan619@gmail.com>
2024-11-05 19:11:01 +0100

    Form validation and controls

--- reviews/forms.py
+++ reviews/forms.py
@@ -1,4 +1,8 @@
 from django import forms 
 
 class ReviewForm(forms.Form):
-    username = forms.CharField(required=True)
+    username = forms.CharField(label="Your Name", max_length=100, error_messages={
+        "required": "Your name should not be empty",
+        "max_length": "Please enter the shorter name"
+    })
+    # captcha_answer = forms.IntegerField(label="2 + 2", label_suffix=" =")
